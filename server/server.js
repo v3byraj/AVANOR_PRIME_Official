3c491ddd25fd94c32ef482d8bd003eaef6a14cc7
Verifier <codeworld2260@gmail.com>
2026-07-26 20:15:30 +0530

    Backend update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173/",
+    "https://avanorprime.netlify.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/innovations", innovationRoutes);
 
