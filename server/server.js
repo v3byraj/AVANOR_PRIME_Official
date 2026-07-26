b753ec5d75d8074a19ed4136af9d4978abb3b65a
Verifier <codeworld2260@gmail.com>
2026-07-26 19:27:21 +0530

    Add express dependency

--- server/server.js
+++ server/server.js
@@ -20,6 +20,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
